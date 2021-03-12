4501cb6dfd7aff015ca226246f78e8f09afa0783
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-03-12 17:10:44 -0500

    trying to figure this out

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,23 +8,23 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 
 // Initialize all of the LayerGroups we'll be using
 var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
+    Earthquake: new L.LayerGroup(),
+    // EMPTY: new L.LayerGroup(),
+    // LOW: new L.LayerGroup(),
+    // NORMAL: new L.LayerGroup(),
+    // OUT_OF_ORDER: new L.LayerGroup()
 };
 
 // Create the map with our layers
 var map = L.map("mapid", {
   center: [37.77986, -122.42905],
-  zoom: 10,
+  zoom: 12,
   layers: [
-    layers.COMING_SOON,
-    layers.EMPTY,
-    layers.LOW,
-    layers.NORMAL,
-    layers.OUT_OF_ORDER
+    layers.Earthquake,
+    // layers.EMPTY,
+    // layers.LOW,
+    // layers.NORMAL,
+    // layers.OUT_OF_ORDER
   ]
 });
 
@@ -33,15 +33,18 @@ lightmap.addTo(map);
 
 // Create an overlays object to add to the layer control
 var overlays = {
-  "Coming Soon": layers.COMING_SOON,
-  "Empty Stations": layers.EMPTY,
-  "Low Stations": layers.LOW,
-  "Healthy Stations": layers.NORMAL,
-  "Out of Order": layers.OUT_OF_ORDER
+  "Earthquake": layers.Earthquake,
+//   "Empty Stations": layers.EMPTY,
+//   "Low Stations": layers.LOW,
+//   "Healthy Stations": layers.NORMAL,
+//   "Out of Order": layers.OUT_OF_ORDER
 };
 
 // Create a control for our layers, add our overlay layers to it
 L.control.layers(null, overlays).addTo(map);
+
+// -----------
+
 
 // Create a legend to display information about our map
 var info = L.control({
