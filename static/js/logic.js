a9ed4cea2d9da07a8d46a45c94479dce1964b8b0
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-03-12 15:28:46 -0500

    initiate

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,7 @@ var myMap = L.map("mapid", {
     zoom: 10
   });
 
-  
+console.log(myMap)
 // Adding tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
