374579cd4c05a9478e145be93758ae4437b8a14a
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-03-12 19:11:49 -0500

    somewhere

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,172 +1,101 @@
-// Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "light-v10",
-  accessToken: API_KEY
-});
+// Creat the Map Box and A layer. Will be modified later. 
 
-// Initialize all of the LayerGroups we'll be using
-var layers = {
-    Earthquake: new L.LayerGroup(),
-    // EMPTY: new L.LayerGroup(),
-    // LOW: new L.LayerGroup(),
-    // NORMAL: new L.LayerGroup(),
-    // OUT_OF_ORDER: new L.LayerGroup()
-};
-
-// Create the map with our layers
-var map = L.map("mapid", {
-  center: [37.77986, -122.42905],
-  zoom: 12,
-  layers: [
-    layers.Earthquake,
-    // layers.EMPTY,
-    // layers.LOW,
-    // layers.NORMAL,
-    // layers.OUT_OF_ORDER
-  ]
-});
-
-// Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
-
-// Create an overlays object to add to the layer control
-var overlays = {
-  "Earthquake": layers.Earthquake,
-//   "Empty Stations": layers.EMPTY,
-//   "Low Stations": layers.LOW,
-//   "Healthy Stations": layers.NORMAL,
-//   "Out of Order": layers.OUT_OF_ORDER
-};
-
-// Create a control for our layers, add our overlay layers to it
-L.control.layers(null, overlays).addTo(map);
-
-// -----------
-
-
-// Create a legend to display information about our map
-var info = L.control({
-  position: "bottomright"
-});
-
-// When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
-};
-// Add the info legend to the map
-info.addTo(map);
-
-// Initialize an object containing icons for each layer group
-var icons = {
-  COMING_SOON: L.ExtraMarkers.icon({
-    icon: "ion-settings",
-    iconColor: "white",
-    markerColor: "yellow",
-    shape: "star"
-  }),
-  EMPTY: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "red",
-    shape: "circle"
-  }),
-  OUT_OF_ORDER: L.ExtraMarkers.icon({
-    icon: "ion-minus-circled",
-    iconColor: "white",
-    markerColor: "blue-dark",
-    shape: "penta"
-  }),
-  LOW: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "orange",
-    shape: "circle"
-  }),
-  NORMAL: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "green",
-    shape: "circle"
-  })
-};
-
-// Perform an API call to the Citi Bike Station Information endpoint
-d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", function(infoRes) {
-
-  // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json", function(statusRes) {
-    var updatedAt = infoRes.last_updated;
-    var stationStatus = statusRes.data.stations;
-    var stationInfo = infoRes.data.stations;
-
-    // Create an object to keep of the number of markers in each layer
-    var stationCount = {
-      COMING_SOON: 0,
-      EMPTY: 0,
-      LOW: 0,
-      NORMAL: 0,
-      OUT_OF_ORDER: 0
-    };
-
-    // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-    var stationStatusCode;
-
-    // Loop through the stations (they're the same size and have partially matching data)
-    for (var i = 0; i < stationInfo.length; i++) {
-
-      // Create a new station object with properties of both station objects
-      var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-      // If a station is listed but not installed, it's coming soon
-      if (!station.is_installed) {
-        stationStatusCode = "COMING_SOON";
-      }
-      // If a station has no bikes available, it's empty
-      else if (!station.num_bikes_available) {
-        stationStatusCode = "EMPTY";
-      }
-      // If a station is installed but isn't renting, it's out of order
-      else if (station.is_installed && !station.is_renting) {
-        stationStatusCode = "OUT_OF_ORDER";
-      }
-      // If a station has less than 5 bikes, it's status is low
-      else if (station.num_bikes_available < 5) {
-        stationStatusCode = "LOW";
-      }
-      // Otherwise the station is normal
-      else {
-        stationStatusCode = "NORMAL";
-      }
-
-      // Update the station count
-      stationCount[stationStatusCode]++;
-      // Create a new marker with the appropriate icon and coordinates
-      var newMarker = L.marker([station.lat, station.lon], {
-        icon: icons[stationStatusCode]
-      });
-
-      // Add the new marker to the appropriate layer
-      newMarker.addTo(layers[stationStatusCode]);
-
-      // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-    }
-
-    // Call the updateLegend function, which will... update the legend!
-    updateLegend(updatedAt, stationCount);
+// The Basis of this code will base on Citi Bike Actiovity and the City Population Actitvity
+var myMap = L.map("mapid", {
+    center: [40.7128, -74.0060],
+    zoom: 13
   });
-});
 
-// Update the legend's innerHTML with the last updated time and station count
-function updateLegend(time, stationCount) {
-  document.querySelector(".legend").innerHTML = [
-    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-  ].join("");
+
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// I want custom icons markers. If i can get it to work
+
+var groudonIcon = L.icon({
+    iconUrl: 'groudon.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+  });
+  
+  var regirockIcon = L.icon({
+    iconUrl: 'regirock.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+  });
+  
+  var tyranitarIcon = L.icon({
+    iconUrl: 'tyranitar.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+  });
+  
+// Grab the JSon
+
+var EarthquakeJSN = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Function to determine a color of the magnitude of the quakes. If i can figure it out, this will be commented to icons
+function MagColor(QuakeColor){
+    switch(QuakeColor){
+        case (0.0 <= QuakeColor && QuakeColor <= 3.0 ):
+            return "yellow";
+        case (3.0 <= QuakeColor && QuakeColor <= 6.0 ):
+            return "magenta";
+        case (6.0 <= QuakeColor && QuakeColor <= 9.0 ):
+            return "indigo"
+        case (9.0 <= QuakeColor && QuakeColor <= 12.0 ):
+            return "purple";
+        case (12.0 <= QuakeColor && QuakeColor <= 20.0 ):
+            return "green"
+        default:
+            return "red"
+    }
+};
+
+
+// Create a function to grab the JSN
+
+d3.json(EarthquakeJSN, function(Quake) {
+
+    // A Variable to create the feature
+    var Qfeatures = Quake.features
+ 
+    console.log(Qfeatures)
+
+    var Magnitude = Qfeatures[0].properties.mag
+    console.log(Magnitude)
+
+    var locations = Qfeatures[0].geometry.coordinates
+    console.log(locations)
+
+    // // Create Circle
+
+    for (var i = 0; i < Qfeatures.length; i++) {
+
+    // Create a circle and pass in some initial options
+    function CirclesMar (Qfeatures, latlng) {
+
+    var circleOption = {
+        color: MagColor(Magnitude) * 10,
+        fillColor: MagColor(Magnitude),
+        fillOpacity: 0.75,
+        opacity: 1
+      }
+      return L.circleMarker(latlng, circleOption) }
+    
+      console.log(CirclesMar)
+
+    // }
+
 }
+});
